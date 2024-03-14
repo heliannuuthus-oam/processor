2d7e7fe8cf562f420bd9ffa46839768cb219512e
heliannuuthus <heliannuuthus@gmail.com>
2024-03-14 23:25:28 +0800

    update cloud.gradle

--- src/main/java/io/ghcr/heliannuuthus/blog/BlogApplication.java
+++ src/main/java/io/ghcr/heliannuuthus/blog/BlogApplication.java
@@ -3,8 +3,9 @@ package io.ghcr.heliannuuthus.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Enable
 @SpringBootApplication
-public class BlogApplication {
+public class DevtoolsApplication {
 
     public static void main(String[] args) {
         // https://docs.spring.io/spring-boot/docs/2.1.5.RELEASE/reference/html/using-boot-devtools.html#using-boot-devtools-restart-disable
