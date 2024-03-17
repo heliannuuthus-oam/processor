ce2cba2c95bbe1f59b257c0179c6ee726e6743b3
heliannuuthus <heliannuuthus@gmail.com>
2024-03-17 20:21:46 +0800

    ed25519 test

--- src/main/java/io/ghcr/heliannuuthus/devtools/crypto/parameters/sm4/SM4ECBParameters.java
+++ src/main/java/io/ghcr/heliannuuthus/devtools/crypto/parameters/sm4/SM4ECBParameters.java
@@ -1,37 +1,36 @@
 package io.ghcr.heliannuuthus.devtools.crypto.parameters.sm4;
 
+import static io.ghcr.heliannuuthus.devtools.crypto.algorithms.Padding.PKCS7;
+
 import io.ghcr.heliannuuthus.devtools.crypto.algorithms.Padding;
 import io.ghcr.heliannuuthus.devtools.crypto.parameters.BlockParameters;
 import lombok.Getter;
 
-import static io.ghcr.heliannuuthus.devtools.crypto.algorithms.Padding.PKCS7;
-
 @Getter
 public class SM4ECBParameters extends BlockParameters {
 
-    public SM4ECBParameters(byte[] key) {
-        this(key, PKCS7);
-    }
+  public SM4ECBParameters(byte[] key) {
+    this(key, PKCS7);
+  }
 
-    protected SM4ECBParameters(byte[] key, Padding padding) {
-        this.key = key;
-        this.padding = padding;
-    }
+  protected SM4ECBParameters(byte[] key, Padding padding) {
+    this.key = key;
+    this.padding = padding;
+  }
 
-    private final Padding padding;
+  private final Padding padding;
 
-    @Override
-    public String getName() {
-        return SM4_ALGORITHM;
-    }
+  @Override
+  public String getName() {
+    return SM4_ALGORITHM;
+  }
 
+  public String getMode() {
+    return ECB_MODE;
+  }
 
-    public String getMode() {
-        return ECB_MODE;
-    }
-
-    @Override
-    public Padding getPadding() {
-        return this.padding;
-    }
+  @Override
+  public Padding getPadding() {
+    return this.padding;
+  }
 }
--- src/main/java/io/ghcr/heliannuuthus/devtools/exception/CryptoException.java
+++ src/main/java/io/ghcr/heliannuuthus/devtools/exception/CryptoException.java
@@ -5,21 +5,21 @@ import org.springframework.web.server.ResponseStatusException;
 
 public class CryptoException extends ResponseStatusException {
 
-    public static final int INIT_MODE = -1;
-    public static final int ENCRYPT_MODE = 1;
-    public static final int DECRYPT_MODE = 2;
-    public static final int SIGN_MODE = 4;
-    public static final int VERIFY_MODE = 8;
+  public static final int INIT_MODE = -1;
+  public static final int ENCRYPT_MODE = 1;
+  public static final int DECRYPT_MODE = 2;
+  public static final int SIGN_MODE = 4;
+  public static final int VERIFY_MODE = 8;
 
-    public CryptoException(int mode, String algorithm) {
-        super(
-                HttpStatus.UNPROCESSABLE_ENTITY, String.format("mode: %d, algorithm: %s", mode, algorithm));
-    }
+  public CryptoException(int mode, String algorithm) {
+    super(
+        HttpStatus.UNPROCESSABLE_ENTITY, String.format("mode: %d, algorithm: %s", mode, algorithm));
+  }
 
-    public CryptoException(int mode, String algorithm, Throwable cause) {
-        super(
-                HttpStatus.UNPROCESSABLE_ENTITY,
-                String.format("mode: %d, algorithm: %s", mode, algorithm),
-                cause);
-    }
+  public CryptoException(int mode, String algorithm, Throwable cause) {
+    super(
+        HttpStatus.UNPROCESSABLE_ENTITY,
+        String.format("mode: %d, algorithm: %s", mode, algorithm),
+        cause);
+  }
 }
